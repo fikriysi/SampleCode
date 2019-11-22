e1e7a03a0cb062dd4410668a123c421a835787e7
Rusdy Hafid Baradja <rusdy@ysi.co.id>
2019-11-22 16:29:31 +0700

    Update Web Form Sample

---
+++ AspNetWebFormsOkta/About.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls; 
+using System.Linq;
+using IdentityModel.Client;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace AspNetWebForms
+{
+    public partial class About : Page
+    { 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+	        string labelText =""; 
+	        if (Request.IsAuthenticated)
+            {
+                var identity = (System.Security.Claims.ClaimsIdentity)Context.User.Identity;
+                int no = 0;
+                labelText = "[";
+                foreach (var c in identity.Claims)
+                {
+                    labelText += (no > 0) ? ",{\"" + c.Type + "\":\"" + c.Value + "\"}" : "{\"" + c.Type + "\":\"" + c.Value + "\"}";
+                    no++;
+                }
+
+                labelText += "]"; 
+               // labelText = identity.Claims.FirstOrDefault(x => x.Type.Equals("access_token")).Value;
+            }
+	        else
+	        {
+		        labelText = "You are not authenticated!";
+	        }
+
+
+            var label = new Label
+	        {
+				Text = labelText
+            };
+
+	        var mainContent = (ContentPlaceHolder)Page.Form.FindControl("MainContent");
+			mainContent.Controls.Add(label);
+        }
+    }
+}
---
+++ AspNetWebFormsOkta/Startup.cs
@@ -0,0 +1,81 @@
+﻿using IdentityModel.Client;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+using Owin;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Security.Claims;
+
+[assembly: OwinStartup(typeof(AspNetWebForms.Startup))]
+
+namespace AspNetWebForms
+{
+    public class Startup
+    {
+        // These values are stored in Web.config. Make sure you update them!
+        private readonly string _clientId = ConfigurationManager.AppSettings["IdProo:ClientId"]; 
+        private readonly string _redirectUri = ConfigurationManager.AppSettings["IdProo:RedirectUri"];
+        private readonly string _authority = ConfigurationManager.AppSettings["IdProo:Authority"];
+        private readonly string _clientSecret = ConfigurationManager.AppSettings["IdProo:ClientSecret"];
+
+        public void Configuration(IAppBuilder app)
+        { 
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            { 
+                ClientId = _clientId,
+                ClientSecret = _clientSecret,
+                Authority = _authority,
+                RedirectUri = _redirectUri,
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                Scope = "profile openid email api.auth user.role user.read user.readAll user.add user.edit application.read application.readAll",
+                TokenValidationParameters = new TokenValidationParameters { NameClaimType = "name" }, 
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                { 
+                    AuthorizationCodeReceived = async n =>
+                    {
+                        // Exchange code for access and ID tokens
+                        var tokenClient = new TokenClient($"{_authority}/connect/token", _clientId, _clientSecret);
+
+                        var tokenResponse = await tokenClient.RequestAuthorizationCodeAsync(n.Code, _redirectUri);
+                        if (tokenResponse.IsError)
+                        {
+                            throw new Exception(tokenResponse.Error);
+                        }
+
+                        var userInfoClient = new UserInfoClient($"{_authority}/connect/userinfo");
+                        var userInfoResponse = await userInfoClient.GetAsync(tokenResponse.AccessToken);
+
+                        var claims = new List<Claim>(userInfoResponse.Claims)
+                        {
+                            new Claim("id_token", tokenResponse.IdentityToken),
+                            new Claim("access_token", tokenResponse.AccessToken)
+                        };
+
+                        n.AuthenticationTicket.Identity.AddClaims(claims);
+                    },
+                },
+            });
+        }
+    }
+}
