48497b6a3e47c090d391411c22cd01737e42cc65
Rusdy Hafid Baradja <rusdy@ysi.co.id>
2020-08-27 16:27:15 +0700

    update simple api core sample

---
+++ SimpleCoreApi/Startup.cs
@@ -0,0 +1,59 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SampleApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors();
+            services.AddDistributedMemoryCache();
+
+            // this API will accept any access token from the authority
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+           .AddIdentityServerAuthentication(options =>
+           {
+               options.Authority = "https://login.com/";
+               options.RequireHttpsMetadata = false;
+           });
+
+            string[] scopes =
+            {
+                "api.auth",
+                "user.read"
+            };
+            services.AddAuthorization(options =>
+            {
+                foreach (var s in scopes)
+                {
+                    options.AddPolicy(s, policy => policy.RequireClaim("scope", s));
+                }
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(policy =>
+            {
+                policy.WithOrigins(
+                    "https://localhost:44300");
+
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            });
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers().RequireAuthorization();
+            });
+        }
+    }
+}
