beca709e871d6b5ccf1326f35ccf4c147d3e40e0
Rennaldy Protono <rennaldy@zeroplus.dev>
2023-03-02 17:44:23 +0700

    fix: basickotlin client

--- BasicKotlin/app/src/main/java/id/idproo/kotlinsample/Shared.kt
+++ BasicKotlin/app/src/main/java/id/idproo/kotlinsample/Shared.kt
@@ -6,8 +6,8 @@ object Shared {
 
 
 object OAuth {
-    const val issuer: String = "https://login.idproo.id"
-    const val clientId: String = "6340830a-396a-4b51-9e61-68f633f0fd7b"
+    const val issuer: String = "https://login.issuer.com"
+    const val clientId: String = "xxxx"
     const val redirect: String = "id.idproo.sample:/oauthredirect"
     const val endSessionRedirect: String = "id.idproo.sample:/"
 }
