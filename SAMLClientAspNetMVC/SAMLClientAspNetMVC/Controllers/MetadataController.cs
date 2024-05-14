77ace29ec6479c5b97a0b539ec61f106e4b8428a
Fikri Firmansyah Akbar <fikri@ysi.co.id>
2024-05-14 16:28:07 +0700

    Sample ASP.NET MVC SAML Client

---
+++ SAMLClientAspNetMVC/SAMLClientAspNetMVC/Controllers/MetadataController.cs
@@ -0,0 +1,75 @@
+﻿using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.MvcCore;
+using ITfoxtec.Identity.Saml2.Schemas;
+using ITfoxtec.Identity.Saml2.Schemas.Metadata;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SAMLClientAspNetMVC.Controllers
+{
+    [AllowAnonymous]
+    [Route("Metadata")]
+    public class MetadataController(Saml2Configuration config) : Controller
+    {
+        public IActionResult Index()
+        {
+            var defaultSite = new Uri($"{Request.Scheme}://{Request.Host.ToUriComponent()}/");
+
+            var entityDescriptor = new EntityDescriptor(config)
+            {
+                ValidUntil = 365,
+                SPSsoDescriptor = new SPSsoDescriptor
+                {
+                    AuthnRequestsSigned = config.SignAuthnRequest,
+                    WantAssertionsSigned = true,
+                    SigningCertificates =
+                [
+                    config.SigningCertificate
+                ],
+                    //EncryptionCertificates = config.DecryptionCertificates,
+                    SingleLogoutServices =
+                [
+                    new SingleLogoutService { Binding = ProtocolBindings.HttpPost, Location = new Uri(defaultSite, "Auth/SingleLogout"), ResponseLocation = new Uri(defaultSite, "Auth/LoggedOut") }
+                ],
+                    NameIDFormats = [NameIdentifierFormats.X509SubjectName],
+                    AssertionConsumerServices =
+                [
+                    new AssertionConsumerService { Binding = ProtocolBindings.HttpPost, Location = new Uri(defaultSite, "Auth/AssertionConsumerService") },
+                ],
+                    AttributeConsumingServices =
+                [
+                    new AttributeConsumingService { ServiceNames = [ new ServiceName("Some SP", "en") ], RequestedAttributes = CreateRequestedAttributes() }
+                ],
+                },
+                Organization = new Organization("Some Organization", "Some Organization Display Name", "http://some-organization.com"),
+                ContactPersons = [
+                new ContactPerson(ContactTypes.Administrative)
+                {
+                    Company = "Some Company",
+                    GivenName = "Some Given Name",
+                    SurName = "Some Sur Name",
+                    EmailAddress = "some@some-domain.com",
+                    TelephoneNumber = "11111111",
+                },
+                new ContactPerson(ContactTypes.Technical)
+                {
+                    Company = "Some Company",
+                    GivenName = "Some tech Given Name",
+                    SurName = "Some tech Sur Name",
+                    EmailAddress = "sometech@some-domain.com",
+                    TelephoneNumber = "22222222",
+                }
+            ]
+            };
+            return new Saml2Metadata(entityDescriptor).CreateMetadata().ToActionResult();
+        }
+
+        private IEnumerable<RequestedAttribute> CreateRequestedAttributes()
+        {
+            yield return new RequestedAttribute("urn:oid:2.5.4.4");
+            yield return new RequestedAttribute("urn:oid:2.5.4.3", false);
+            yield return new RequestedAttribute("urn:xxx", "test-value");
+            yield return new RequestedAttribute("urn:yyy", "123") { AttributeValueType = "xs:integer" };
+        }
+    }
+}
---
+++ SAMLClientAspNetMVC/SAMLClientAspNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAMLClientAspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
